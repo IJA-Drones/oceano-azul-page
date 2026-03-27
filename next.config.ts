e48df9d41f1cedd4095019b2106e821c2c5725af
pedro-cruzz <pedrocruzvb123@gmail.com>
2026-03-27 11:32:13 -0300

    feat: configuração para hospedagem

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: false
+  output: 'export',
 };
 
 export default nextConfig;
